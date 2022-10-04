11db60efd424067c2f7cedfee9df3ca69388a22d
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-10-04 11:36:56 +0530

    changes

--- controllers/posts.js
+++ controllers/posts.js
@@ -9,7 +9,7 @@ export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
                 
-        res.status(200).json(postMessages).stringfy();
+        res.status(200).json.stringfy(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
